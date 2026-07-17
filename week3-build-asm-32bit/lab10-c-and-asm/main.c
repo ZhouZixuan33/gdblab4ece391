2d732cf850906779a21f2928f2ea5c9b078b279d
ZhouZixuan33 <zhou_zixuan@outlook.com>
2026-07-17 17:58:35 +0800

    Migrate Lab10 to RV64 calling convention

--- week3-build-asm-32bit/lab10-c-and-asm/main.c
+++ week3-build-asm-32bit/lab10-c-and-asm/main.c
@@ -1,9 +1,12 @@
+long asm_add_three(long first, long second, long third);
 int check_preserves_s1(void);
 
+volatile long g_add_result;
 volatile int g_check_result;
 volatile int g_done;
 
 int main(void) {
+    g_add_result = asm_add_three(100, 20, 1);
     g_check_result = check_preserves_s1();
     g_done = 1;
 
